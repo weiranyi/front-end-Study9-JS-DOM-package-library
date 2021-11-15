b9532ad72c3d3f8591ea47855d0af38234c1b3a6
weiranyi <weiranyi@vip.qq.com>
2021-11-15 09:50:01 +0800

    保留作业要求部分其余代码注释掉

--- src/main.js
+++ src/main.js
@@ -1,56 +1,62 @@
-// 创建节点
-const div = dom.create('<td>12</td>');
-console.log(div);
+const div = dom.find('#test>.red')[0] // 获取对应的元素
+dom.style(div, 'color', 'red') // 设置 div.style.color
 
-// 添加弟弟节点
-dom.after(test,div);
-
+const divList = dom.find('.red') // 获取多个 div.red 元素
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+// // 创建节点
+// const div = dom.create('<td>12</td>');
+// console.log(div);
 //
-const div3 = dom.create('<div id="parent"></div>')
-dom.wrap(test,div3);
+// // 添加弟弟节点
+// dom.after(test,div);
+//
+// //
+// const div3 = dom.create('<div id="parent"></div>')
+// dom.wrap(test,div3);
+//
+// // 删除节点
+// const nodes = dom.empty(window.empty);
+// console.log(nodes);
+//
+// // 实现写操作
+// dom.attr(test,'title',"hi,I'm weiranyi");
+// // 实现读操作
+// const title = dom.attr(test,'title');
+// // 输出到控制台
+// console.log(`title:${title}`);
+// // 写文本操作
+// dom.text(test,'您好，这是新的内容');
+// // 读取文本操作
+// dom.text(test);
+//
+// // 改样式
+// dom.style(test,{border:'1px solid red',color:'blue'});
+// console.log(dom.style(test,'border')); // 读取border的值
+// dom.style(test,'border','1px solid black');
+//
+// //修改样式
+// dom.class.add(test,'red');
+// dom.class.add(test,'blue');
+// dom.class.remove(test,'blue');
+// console.log(dom.class.has(test,'blue'))
+//
+// // 事件
+// const fn = ()=>{
+//     console.log('点击了');
+// }
+// dom.on(test,'click',fn)
+// dom.off(test,'click',fn)
+//
+// const testDiv = dom.find('#test')[0]
+// console.log(testDiv)
+// const testDiv2 = dom.find('#test2')[0]
+// console.log(dom.find('.red',testDiv2)[0])
+// console.log(dom.parent(test))
+// console.log(dom.siblings(dom.find('#s2')[0]));
+// console.log(dom.next(dom.find('#s2')[0]));
+// console.log(dom.previous(dom.find('#s2')[0]));
+//
+// const t = dom.find('#travel')[0]
+// dom.each(dom.children(t),(n)=>dom.style(n,'color','red'))
+// console.log(dom.index(dom.find('#s2')[0]))
 
-// 删除节点
-const nodes = dom.empty(window.empty);
-console.log(nodes);
-
-// 实现写操作
-dom.attr(test,'title',"hi,I'm weiranyi");
-// 实现读操作
-const title = dom.attr(test,'title');
-// 输出到控制台
-console.log(`title:${title}`);
-// 写文本操作
-dom.text(test,'您好，这是新的内容');
-// 读取文本操作
-dom.text(test);
-
-// 改样式
-dom.style(test,{border:'1px solid red',color:'blue'});
-console.log(dom.style(test,'border')); // 读取border的值
-dom.style(test,'border','1px solid black');
-
-//修改样式
-dom.class.add(test,'red');
-dom.class.add(test,'blue');
-dom.class.remove(test,'blue');
-console.log(dom.class.has(test,'blue'))
-
-// 事件
-const fn = ()=>{
-    console.log('点击了');
-}
-dom.on(test,'click',fn)
-dom.off(test,'click',fn)
-
-const testDiv = dom.find('#test')[0]
-console.log(testDiv)
-const testDiv2 = dom.find('#test2')[0]
-console.log(dom.find('.red',testDiv2)[0])
-console.log(dom.parent(test))
-console.log(dom.siblings(dom.find('#s2')[0]));
-console.log(dom.next(dom.find('#s2')[0]));
-console.log(dom.previous(dom.find('#s2')[0]));
-
-const t = dom.find('#travel')[0]
-dom.each(dom.children(t),(n)=>dom.style(n,'color','red'))
-console.log(dom.index(dom.find('#s2')[0]))
